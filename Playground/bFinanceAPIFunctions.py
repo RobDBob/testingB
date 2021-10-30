7ddf27b16a477198168ac959b3fc89e48e6a7883
RobDBob <r.deringer@live.com>
2021-10-30 21:54:50 +0100

    update, refactored magic numbers to properties

--- Playground/bFinanceAPIFunctions.py
+++ Playground/bFinanceAPIFunctions.py
@@ -7,10 +7,10 @@ def function():
         print(client.get_ticker(symbol="BTCUSDT"))
         sleep(5)
 
-def getClient():
+def getClient(test_net):
     api_key_test = "f1itPrLonskTkpBVZVasE8BodXjrGI3FVkAmnqr5CxIt4MtZhpIWoMRDx9ZGF8CL"
     api_secret_test = "PhhYD2agY19f8ZngoOR588ZsuBuKjONctjbABtNXXUXD8qMdw1EWMNYC4fTB2w6W"
-    return Client(api_key=api_key_test, api_secret=api_secret_test, testnet=True)
+    return Client(api_key=api_key_test, api_secret=api_secret_test, testnet=test_net)
 
 if __name__ == "__main__":
     function()
--- newAttempt.py
+++ newAttempt.py
@@ -1,7 +1,6 @@
-from binance import ThreadedWebsocketManager
+from binance import ThreadedWebsocketManager, helpers
 from Playground.bFinanceAPIFunctions import getClient
 import os
-from binance.client import Client
 import pandas as pd
 import datetime, time
 import pandas_ta as ta
@@ -10,15 +9,16 @@ from main1dbstuff import saveToFile, readFromFile
 class test:
     all_columns = ['dateTime', 'open', 'high', 'low', 'close', 'volume', 'closeTime', 'quoteAssetVolume', 'numberOfTrades', 'takerBuyBaseVol', 'takerBuyQuoteVol', 'ignore']
     columns_to_drop = ['closeTime', 'quoteAssetVolume', 'numberOfTrades', 'takerBuyBaseVol','takerBuyQuoteVol', 'ignore']
-
     # ['dateTime', 'open', 'high', 'low', 'close', 'volume']
     columns_to_keep = ['dateTime', 'open', 'high', 'low', 'close', 'volume']
-
     date_time_format = '%Y-%m-%d %H:%M:%S'
-    symbol = "BTCUSDT"
 
-    def __init__(self, minutes):
-        self.df = self.getHistoricalData(minutes)
+    def __init__(self, symbol, historic_minutes, kline_interval, test_net):
+        self.symbol = symbol
+        self.kline_interval = kline_interval
+        self.test_net = test_net
+
+        self.df = self.getHistoricalData(historic_minutes)
 
     def _get_df_from_candle(self, candle):
         """
@@ -37,19 +37,19 @@ class test:
         return datetime.datetime.utcfromtimestamp((int(date_time_stamp_ms)/1000)).strftime(self.date_time_format)
 
     def getHistoricalData(self, howLongMinutes):
-        client = getClient()
+        client = getClient(self.test_net)
         # Calculate the timestamps for the binance api function
         untilThisDate = datetime.datetime.utcnow()
         sinceThisDate = untilThisDate - datetime.timedelta(minutes = howLongMinutes)
         # Execute the query from binance - timestamps must be converted to strings !
-        candle = client.get_historical_klines(self.symbol, Client.KLINE_INTERVAL_3MINUTE, str(sinceThisDate), str(untilThisDate))
+        candle = client.get_historical_klines(self.symbol, self.kline_interval, str(sinceThisDate), str(untilThisDate))
         
         df = self._get_df_from_candle(candle)
         df.columns = self.all_columns
 
         
         # # as timestamp is returned in ms, let us convert this back to proper timestamps.
-        df.dateTime = pd.to_datetime(df.dateTime, unit='ms').dt.strftime(self.date_time_format) # self._get_datetime_series(df.dateTime)
+        # df.dateTime = pd.to_datetime(df.dateTime, unit='ms').dt.strftime(self.date_time_format) # self._get_datetime_series(df.dateTime)
         df.set_index('dateTime', inplace=True)
 
         # Get rid of columns we do not need
@@ -69,21 +69,22 @@ class test:
             self.locked = True
             # series = [self._get_datetime_single(msg['E']), msg['o'], msg['h'], msg['l'], msg['v']]
             # df = pd.Series(series)
-            df = pd.DataFrame([[self._get_datetime_single(msg['E']), msg['o'], msg['h'], msg['l'], msg['c'], msg['v']]])
+            df = pd.DataFrame([[msg['E'], msg['o'], msg['h'], msg['l'], msg['c'], msg['v']]])
             df.columns = self.columns_to_keep
             df.set_index('dateTime', inplace=True)
             self.df = self.df.append(df) # , ignore_index=True)
 
             print("__________________________")
+            # self.df.tail(1).dateTime
             print(self.df)
         finally:
             self.locked = False
 
-
-data_frame = test(minutes=10)
+# test_net = True
+data_frame = test(symbol="BTCUSDT", historic_minutes=12, kline_interval="3m", test_net=True)
 
 def main():
-    twm = ThreadedWebsocketManager(testnet=True)
+    twm = ThreadedWebsocketManager(testnet=data_frame.test_net)
     twm.daemon = True
     twm.start()
     twm.start_symbol_miniticker_socket(callback=data_frame.update_dataframe, symbol=data_frame.symbol)
