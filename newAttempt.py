7fd3d27b461b4a2538cd8ef50fba78709001acc9
RobDBob <r.deringer@live.com>
2021-11-03 18:16:00 +0000

    working with approach to df-ing

--- newAttempt.py
+++ newAttempt.py
@@ -17,8 +17,7 @@ logger_name_high_level = "WebSocketPriceUpdaterTester"
 create_logger(logger_name_high_level, "TestRecords.log")
 
 class TAAnalyzer:
-    def __init__(self, symbol, kline_interval, historic_data=None):
-        self.symbol = symbol
+    def __init__(self, kline_interval, historic_data=None):
         self.kline_interval = kline_interval
 
         if historic_data is None:
@@ -34,7 +33,7 @@ class TAAnalyzer:
         return pd.to_datetime(date_time_stamps_ms, unit='ms').dt.strftime(const.date_time_format)
 
     def do_stuff(self, msg):
-        if (self.update_dataframe(msg)):
+        if (self.update_test_data(msg)):
             buy = []
             sell = []
             buy.append(self.taTester.rsi_test(ta.rsi(self.df["close"], length=12), buy=True))
@@ -66,23 +65,32 @@ class TAAnalyzer:
         # self.logger(pprint(order))
         return
 
-    def callback_update_to_df(self, msg):
-        # data = {
-        #     "dateTime": msg['E'], 
-        #     "open": float(msg['k']['o']), 
-        #     "high": float(msg['k']['h']), 
-        #     "low": float(msg['k']['l']), 
-        #     "close": float(msg['k']['c']), 
-        #     "volume":float(msg['k']['v'])}
+    def callback_update_to_df_from_kline(self, msg):
+        data = {
+            "dateTime": [msg['E']], 
+            "open": [float(msg['k']['o'])], 
+            "high": [float(msg['k']['h'])], 
+            "low": [float(msg['k']['l'])], 
+            "close": [float(msg['k']['c'])], 
+            "volume": [float(msg['k']['v'])]}
+        df = self.convert_to_df(data)
+        self.update_test_data(df)
 
         # ds = pd.Series(data=data, index="dateTime")
-        df = pd.DataFrame([[msg['E'], float(msg['k']['o']), float(msg['k']['h']), float(msg['k']['l']), float(msg['k']['c']), float(msg['k']['v'])]])
-        df.columns = const.columns_to_keep
+        # columns_to_keep = ['dateTime', 'open', 'high', 'low', 'close', 'volume']
+
+    def convert_to_df(self, data):
+        """
+        data: dict
+        key: string: column names
+        value: list: column values
+        """
+        df = pd.DataFrame(data)
         df.set_index('dateTime', drop=False, inplace=True)
         df.dateTime = DateHelper.get_datetime_single(df.dateTime)
-        self.update_dataframe(df)
+        return df
 
-    def update_dataframe(self, df):
+    def update_test_data(self, df):
         # ONLY UPDATE IF LANDS ON EXPECTED INTERVAL 
         interval_milliseconds = helpers.interval_to_milliseconds(self.kline_interval)
         id_time_stamp_near_interval = datetime.utcfromtimestamp(df.index[-1]/1000) > datetime.utcfromtimestamp((self.df.index[-1] + interval_milliseconds)/1000)
@@ -93,7 +101,6 @@ class TAAnalyzer:
                 self.df = self.df.append(df)
 
                 print(f"Updated: {df.loc[df.index[-1], 'dateTime']}, new price: {df.loc[df.index[-1], 'close']}")
-                print(self.df)
 
             except Exception as e:
                 print(e)
@@ -109,23 +116,22 @@ class TAAnalyzer:
             return False
 
 
-def start_web_socket(args, data_updater, test_net):
-    twm = ThreadedWebsocketManager(testnet=test_net)
+def start_web_socket(args, data_updater):
+    twm = ThreadedWebsocketManager(testnet=args.test_net)
     twm.daemon = True
     twm.start()
     #twm.start_symbol_miniticker_socket(callback=data_frame.update_df, symbol=data_frame.symbol)
-    twm.start_kline_socket(callback=data_updater.callback_update_to_df, symbol=data_updater.symbol)
+    twm.start_kline_socket(callback=data_updater.callback_update_to_df_from_kline, symbol=args.symbol)
     twm.join(timeout=args.timeout)
 
 def run_test_on_existind_data(args):
+    df = pd.read_pickle("2weekWorthBTCUSDT3m.pkl")
+    initial_df = df[:60]
+    later_df = df[60:]
+    data_updater = TAAnalyzer(kline_interval=args.interval, historic_data=initial_df)
     
-    # df = pd.read_json("weekWorthBTCUSDT3m.json")
-    # initial_df = df[60:]
-    # later_df = df[:60]
-    # data_updater = TAAnalyzer(symbol=args.symbol, kline_interval=interval, historic_data=initial_df)
-    # for dx in range(len(later_df)):
-    #     later_df
-    #     [dfa.iloc[k] for k in range(len(dfa))]
+    for dx in range(len(later_df)):
+        data_updater.update_test_data(later_df.iloc[dx])
     return
 
 def run_of_websocket(args):
@@ -134,8 +140,8 @@ def run_of_websocket(args):
     """
     client = getClient(test_net=args.test_net)
     historic_data = APIHelper.get_historical_data(client, args.symbol, howLongMinutes=60, kline_interval=args.interval)
-    data_updater = TAAnalyzer(symbol=args.symbol, kline_interval=args.interval, historic_data=historic_data)
-    start_web_socket(args, data_updater, args.test_net)
+    data_updater = TAAnalyzer(kline_interval=args.interval, historic_data=historic_data)
+    start_web_socket(args, data_updater)
 
 
 if __name__ == "__main__":
