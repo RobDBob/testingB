9c6bbc84c51a3c793f37aedfcbe453eb068ff42e
Robert Deringer <r.deringer@live.com>
2021-12-07 08:09:22 +0000

    code tidy up

--- BlackBoxScripts/BinanceAllKlinesToNotification.py
+++ BlackBoxScripts/BinanceAllKlinesToNotification.py
@@ -1,23 +1,52 @@
 import pandas as pd
 import logging
+import pandas_ta as ta
 from binance import enums
 from binance import ThreadedWebsocketManager
 from binanceHelper.bFinanceAPIFunctions import getClient
+from Helpers.DateHelper import get_datetime_single
 
-# def create_logger(logger_name, file_name=None):
-#     logger = logging.getLogger(logger_name)
-#     logger.setLevel(logging.DEBUG)
-#     file_handler = logging.FileHandler(file_name)
-#     formatter    = logging.Formatter('%(asctime)s(%(levelname)s): %(message)s')
-#     file_handler.setFormatter(formatter)
-#     logger.addHandler(file_handler)
-#     return logging.getLogger(logger_name)
+def create_logger(logger_name, file_name=None):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(file_name)
+    formatter    = logging.Formatter('%(asctime)s(%(levelname)s): %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logging.getLogger(logger_name)
 
-# logger = create_logger(__name__, "LOG_binancealltickers.log")
+logger = create_logger(__name__, "LOG_binanceKline.log")
 
 class ProcessData:
     def __init__(self):
         self.full_klines_data = {}
+        self.anomaly_detected_timestamp = {}
+        
+        # mark purchase, so we'll need time, timestop
+        self.purchased_symbol = {}
+
+    def check_for_anomaly(self, symbol, number_of_trades, volume, time_stamp):
+        # check if already was hinted
+        if symbol in self.anomaly_detected_timestamp and self.anomaly_detected_timestamp[symbol] + 3600 > time_stamp:
+            logger.info(f"{get_datetime_single(time_stamp)}: anomally previously detected {symbol} - we are on the pause until after {self.anomaly_detected_timestamp[symbol] + 3600}")
+            return False
+
+        symbol_data = self.full_klines_data.get(symbol, None)
+        if symbol_data is None:
+            # no data available to compare yet
+            return False
+
+        anomaly_detected = (symbol_data.tail(1).volSMA * 8 > volume).bool() & (symbol_data.tail(1).NOTSMA * 8 > number_of_trades).bool()
+        
+        if anomaly_detected:
+            logger.info(f"{symbol} - anomaly detected")
+            self.anomaly_detected_timestamp[symbol] = time_stamp
+
+        return anomaly_detected
+
+    def notify(self, symbol, price, time_stamp):
+        logger.info(f"{get_datetime_single(time_stamp)}: NOTIFICATION: {symbol}, price: {price}")
+        return
 
     def process_msg(self, msg):
         """
@@ -27,33 +56,33 @@ class ProcessData:
 
         is_kline_complete = msg["data"]["k"]["x"]
         symbol = msg["data"]["s"]
+        number_of_trades = msg["data"]["k"]["n"]
+        volume = round(float(msg["data"]["k"]["v"]), 4)
+        close = round(float(msg["data"]["k"]["c"]), 4)
+        time_stamp = msg["data"]["k"]["T"]/1000
 
         if not is_kline_complete:
-            if self.is_volume_an_anomaly():
-                self.notify()
+            if self.check_for_anomaly(symbol, number_of_trades, volume, time_stamp):
+                self.notify(symbol, close, time_stamp)
             return
 
-        close_price = round(float(msg["data"]["k"]["c"], 4))
-        number_of_trades = msg["data"]["k"]["n"]
-        base_asset_volume = round(float(msg["data"]["k"]["v"], 4))
-        taker_buy_base_asset_volume = round(float(msg["data"]["k"]["V"], 4))
-        taker_buy_quote_asset_volume = round(float(msg["data"]["k"]["Q"], 4))
-
         data = {
-            "symbol": msg["data"]["s"],
-            "klineEndTime": msg["data"]["k"]["T"]/1000,
-            "close": close_price, 
-            "numberOfTrades": number_of_trades, 
-            "baseVolume": base_asset_volume, 
-            "takerBaseVolume": taker_buy_base_asset_volume,
-            "takerQuoteVolume": taker_buy_quote_asset_volume}
+            "timeStamp": time_stamp,
+            "close": close, 
+            "volume": volume, 
+            "numberOfTrades": number_of_trades}
 
-        self.save_data(data)
+        self.save_data(data, symbol)
 
-    def save_data(self, data):
-        if data["symbol"] not in self.full_klines_data:
-            self.full_klines_data[data["symbol"]] = pd.DataFrame()
-        self.full_klines_data[data["symbol"]].append(data)
+    def save_data(self, data, symbol):
+        logger.info(f"Symbol: {symbol} - new data added")
+        if symbol not in self.full_klines_data:
+            self.full_klines_data[symbol] = pd.DataFrame()
+
+        self.full_klines_data[symbol]=self.full_klines_data[symbol].append(data, ignore_index=True)
+        print(self.full_klines_data)
+        self.full_klines_data[symbol]["volSMA"]=ta.sma(self.full_klines_data[symbol].volume, length=60)
+        self.full_klines_data[symbol]["NOTSMA"]=ta.sma(self.full_klines_data[symbol].numberOfTrades, length=60)
 
 
 processData = ProcessData()
@@ -69,13 +98,15 @@ def start_web_socket():
     """
     listen to websocket, populate postgresql with result
     """
-    twm = ThreadedWebsocketManager(testnet=False)
+    streams = get_kline_steam_names()[:3]
+    logger.info(streams)
+    twm = ThreadedWebsocketManager()
     twm.daemon = True
-    twm.start_multiplex_socket(callback=processData.process_msg, streams=get_kline_steam_names())
-    twm.join(60)
+    twm.start()
+    twm.start_multiplex_socket(callback=processData.process_msg, streams=streams)
+    twm.join(120)
     twm.stop()
 
 if __name__ == "__main__":
     # https://binance-docs.github.io/apidocs/spot/en/#all-market-tickers-stream
-
     start_web_socket()
--- Helpers/DateHelper.py
+++ Helpers/DateHelper.py
@@ -6,6 +6,9 @@ import pandas as pd
 def get_datetime_single_from_ms(date_time_stamp_ms):
     return datetime.utcfromtimestamp((int(date_time_stamp_ms)/1000)).strftime(const.date_time_format)
 
+def get_datetime_single(date_time_stamp):
+    return datetime.utcfromtimestamp((int(date_time_stamp))).strftime(const.date_time_format)
+
 def get_datetime_series(date_time_stamps_ms):
     # print(f"---- type: {type(date_time_stamps_ms)}, value: {date_time_stamps_ms}")
     return pd.to_datetime(date_time_stamps_ms, unit='ms').dt.strftime(const.date_time_format)
